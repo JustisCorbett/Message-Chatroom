821e372fba91252060415a973883824ed07ce496
unknown <corbettjustis@gmail.com>
2020-04-14 00:23:20 -0600

    design changes

--- static/index.js
+++ static/index.js
@@ -1,9 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
     const roomPanel = document.querySelector(".room-panel");
+    const panelArrow = document.querySelector("#panel-arrow");
     document.querySelector('.panel-button').onclick = () => {
-        if (roomPanel.classList.contains("panel-open"))
+        if (roomPanel.classList.contains("panel-open")){
             roomPanel.classList.remove("panel-open");
-        else
+            document.getElementById("panel-arrow").innerHTML = ">";
+        }
+        else {
             roomPanel.classList.add("panel-open");
+            document.getElementById("panel-arrow").innerHTML = "<";
+        }
     };
 });
