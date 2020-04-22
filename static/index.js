093244a1a8df5c43408da3a6645f72ab3701c93f
unknown <corbettjustis@gmail.com>
2020-04-22 17:01:01 -0600

    styling headers

--- static/index.js
+++ static/index.js
@@ -1,10 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
     const roomPanel = document.querySelector(".room-panel");
-    const panelArrow = document.querySelector("#panel-arrow");
     document.querySelector('.panel-button').onclick = () => {
         if (roomPanel.classList.contains("panel-open")){
             roomPanel.classList.remove("panel-open");
             document.getElementById("panel-arrow").innerHTML = ">";
+            
         }
         else {
             roomPanel.classList.add("panel-open");
