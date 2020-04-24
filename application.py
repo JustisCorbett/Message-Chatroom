a7c17238c64e2bc3f57fcf5eac4ed50d323e1262
unknown <corbettjustis@gmail.com>
2020-04-24 16:55:32 -0600

    some socket connections

--- application.py
+++ application.py
@@ -1,7 +1,7 @@
 import os
 
 from flask import Flask, render_template, session
-from flask_socketio import SocketIO, emit
+from flask_socketio import SocketIO, emit, join_room, leave_room
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
@@ -12,7 +12,7 @@ rooms = []
 def index():
     """render main page"""
 
-    try: 
+    try:
         username = session["username"]
     except KeyError:
         username = None
@@ -21,8 +21,23 @@ def index():
     except KeyError:
         lastRoom = None
 
-    return render_template("index.html", username=username, lastChannel=lastChannel, rooms=rooms)
+    return render_template("index.html", username=username, lastChannel=lastRoom, rooms=rooms)
 
+@socketio.on('join')
+def on_join(data):
+    username = data['username']
+    room = data['room']
+    join_room(room)
+    send(username + ' has entered the room.', room=room)
 
+@socketio.on('leave')
+def on_leave(data):
+    username = data['username']
+    room = data['room']
+    leave_room(room)
+    send(username + ' has left the room.', room=room)
+
+@socketio.on("changeRoom")
+def changeRoom(arg1)
 if __name__ == "__main__":
     socketio.run(app)
