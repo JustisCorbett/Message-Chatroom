7f6caefb7154495769a59bccb525e3579b617ee4
unknown <corbettjustis@gmail.com>
2020-05-11 19:07:47 -0600

    cleaned formatting

--- application.py
+++ application.py
@@ -1,7 +1,7 @@
 import os
 
-from flask import Flask, render_template, session
-from flask_socketio import SocketIO, emit, join_room, leave_room
+from flask import Flask, render_template, session, request, jsonify, make_response
+from flask_socketio import SocketIO, emit, join_room, leave_room, send
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
@@ -27,27 +27,29 @@ def index():
 
 
 @app.route("/create-name", methods=["POST"])
-def createName:
+def createName():
     return "thanks"
 
 
-@socketio.on('join')
+@socketio.on("join")
 def on_join(data):
-    username = data['username']
-    room = data['room']
+    username = data["username"]
+    room = data["room"]
     join_room(room)
-    send(username + ' has entered the room.', room=room)
+    send(username + " has entered the room.", room=room)
 
 
-@socketio.on('leave')
+@socketio.on("leave")
 def on_leave(data):
-    username = data['username']
-    room = data['room']
+    username = data["username"]
+    room = data["room"]
     leave_room(room)
-    send(username + ' has left the room.', room=room)
+    send(username + " has left the room.", room=room)
 
 
 @socketio.on("changeRoom")
-def changeRoom(arg1)
+def changeRoom(arg1):
+    return ("change")
+
 if __name__ == "__main__":
     socketio.run(app)
